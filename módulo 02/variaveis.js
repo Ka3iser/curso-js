e04762c2ca16cd5fd49d545e369dcc8fcbd45657
Ka3iser <henrideve3@gmail.com>
2025-03-19 14:57:49 -0300

    voltando a aprender

---
+++ módulo 02/variaveis.js
@@ -0,0 +1,25 @@
+/*
+    regras para criar variaveis
+    - podem começar com letra, $ ou _
+    - não pode começar com números
+    - é possivel usar letras ou numeros
+    - é possivel usar acentos e simbolos
+    - não podem conter espaços
+    - não podem ser palavras reservadas
+*/
+
+/* 
+    dicas pra nomes de var
+    - maiusculas e minusculas fazem diferença
+    - tente escolher nomes coerentes
+    - evite se tornar um programador alfabeto ou um programador contador
+*/
+
+//em js existem tipos de dados
+- string // palavras q tem "" '' e ``
+- boolean // verdadeiro ou falso
+- number // numeros
+- null
+- undefined
+- object
+- function
